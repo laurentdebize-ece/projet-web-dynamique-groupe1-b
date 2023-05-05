2f7db69484a2d09a0c76a5b4b609512178a77839
erwanpillard <erwan.pillard@edu.ece.fr>
2023-05-05 13:32:08 +0200

    update organisation fichier

---
+++ navbar.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="index.php">
+            <img src="logo_omnesBox.png" alt="Logo" class="navbar-brand-img">
+        </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Accueil</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="cartes_cadeau.php" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Cartes cadeau
+                </a>
+                
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <div class="dropdown-header">Par montant</div>
+                    <a class="dropdown-item" href="cartes_cadeau.php">25€</a>
+                    <a class="dropdown-item" href="cartes_cadeau.php">50€</a>
+                    <a class="dropdown-item" href="cartes_cadeau.php">100€</a>
+                    <div class="dropdown-divider"></div>
+                    <div class="dropdown-header">Par activité</div>
+                    <a class="dropdown-item" href="cartes_cadeau.php">Restaurant</a>
+                    <a class="dropdown-item" href="cartes_cadeau.php">Spa</a>
+                    <a class="dropdown-item" href="cartes_cadeau.php">Aventure</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="panier.php">Panier</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link omnes-box" href="omnes_box.php">J'ai une OmnesBox</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="mon_compte.php">Mon compte</a>
+            </li>
+        </ul>
+    </div>
+</nav>
