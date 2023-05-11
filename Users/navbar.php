965ad05b4f6b9ea9fe5262c2371bf448decd2238
erwanpillard <erwan.pillard@edu.ece.fr>
2023-05-11 13:06:08 +0200

    verif connexion

--- Users/navbar.php
+++ Users/navbar.php
@@ -1,38 +1,25 @@
 <style>
 
 
-.deconnexion {
+    .deconnexion {
 
-border-radius: 15px;
+    border-radius: 15px;
 
-color: white;
-font-family: Arial, Helvetica, sans-serif;
-font-size: 15x;
-font-weight: bold;
+    color: white;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 15x;
+    font-weight: bold;
 
-}
+    }
 
-.deconnexion:hover {
+    .deconnexion:hover {
 
 
-border: #176ad5;
-color:white;    
-}
+    border: #176ad5;
+    color:white;    
+    }
 </style>
 
-<?php include("verif_session.php") ?>
-
-<script>
-    
-    function deconnexion() {
-        $_SESSION['connected'] = false ;
-        unset($_SESSION['id']) ;
-        unset($_SESSION['nom']) ;
-        unset($_SESSION['prenom']) ;
-        unset($_SESSION['email']) ;
-    }
-    
-</script>
 
 <nav class="navbar navbar-expand-lg navbar-dark">
         <a class="navbar-brand" href="accueil.php">
@@ -73,24 +60,24 @@ color:white;
         
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-            <?php 
-                if($_SESSION['connected']){
-                    //echo '<a type="button" class="nav-link" onclick="deconnexion()">Déconnexion</a>';
+            <?php
+                // vérifier si l'utilisateur est connecté
+                if(isset($_SESSION['connected']) && $_SESSION['connected']){
                     echo '<a href="deconnexion.php">Déconnexion</a>';
                 }
-                ?> 
+            ?>
+            
                 </li>
         <li class="nav-item">
-                <?php
-                if(!$_SESSION['connected']){
-                    echo '<a class="nav-link" href="connexion.php">Connexion</a>' ;
-                }
-                else {
+            <?php
+                if(isset($_SESSION['connected']) && $_SESSION['connected']){
                     echo '<a class="nav-link" href="mon_compte.php">Mon Compte</a>' ;
                 }
-                ?>
+                else {
+                    echo '<a class="nav-link" href="connexion.php">Connexion</a>' ;
+                }
+            ?>
             </li>
-        </ul>
-                
+        </ul>   
     </div>
 </nav>
--- Users/panier.php
+++ Users/panier.php
@@ -7,18 +7,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     
-    <?php
-        session_start();
 
-        // Vérifier si l'utilisateur est connecté
-        if (!isset($_SESSION['id'])) {
-        // Rediriger vers la page de connexion
-        header("Location: connexion.php");
-        exit;
-        }
-    ?>
-
-    
     <script>
         function updateCartDisplay() {
             const cartContainer = document.getElementById("cartContainer");
