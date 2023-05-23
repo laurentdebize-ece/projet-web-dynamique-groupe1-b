c13599e1c0e9de79c6ba566eb1174defe3ea0789
Nelman4528 <nelman4528@gmail.com>
2023-05-23 21:49:57 +0200

    Amelioration

--- Partenaire/activation_mdp.php
+++ Partenaire/activation_mdp.php
@@ -7,7 +7,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link href="../CSS/modifier_MDP.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="../CSS/activation_mdp.css" rel="stylesheet" type="text/css" media="all" />
     <?php include("../Users/verif_connexion_bdd.php") ?>
     <?php include("../Users/verif_session.php") ?>
 
