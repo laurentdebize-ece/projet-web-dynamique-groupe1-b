f02111cee5c8c98d0ebd45a2a987a31c2f898205
Nelman4528 <nelman4528@gmail.com>
2023-05-23 11:43:53 +0200

    Activation mdp

--- Partenaire/activation_mdp.php
+++ Partenaire/activation_mdp.php
@@ -49,64 +49,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon compte - OMNES BOX</title>
-    <style>
-        .container {
-            max-width: 960px;
-            margin: 50px auto;
-            background-color: white;
-            box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-            border-radius: 5px;
-        }
-
-        .btn {
-            background-color: rgb(38, 93, 155);
-            color: white;
-            font-size: 16px;
-            font-weight: bold;
-            border: none;
-            padding: 10px 20px;
-            border-radius: 5px;
-            transition: all 0.3s ease;
-            margin-top: 20px;
-        }
-
-        .btn:hover {
-            background-color: #f0cb64;
-            color: black;
-            text-decoration: none;
-        }
-    </style>
 </head>
 
 <body>
     <?php include('navbar_partenaire.php') ?>
     <br><hr>
     <div class="container">
-        <div class="row">
-            <div class="col-sm-3"></div>
-            <div class="col-sm-6" style="height:50%;">
-                <div class="panel panel-default" style="height:450px">
-                    <br><br>
-                    <h2 class="text">Activer votre compte</h2>
-                    <?php echo "<p style='text-align:center;color:red'>" . $email_error . "</p>" ?>
-                    <form action="" method="post">
-                        <div class="pieddepage">
-                            <p class="text"> Nouveau mot de passe :</p>
-                        </div>
-                        <input class="log" name="nouveauMDP" id="nouveauMDP"> </span>
-                        <br>                        <br>
-
-                        <div class="pieddepage">
-                            <p class="text"> Confirmer votre mot de passe : </p>
-                        </div>
-                        <input class="log" name="confirmationMDP" id="confirmationMDP">
-                        <br>
-                        <input class="btn" type="submit" value="Connexion"></input>
-                    </form>
-                </div>
-            </div>
-        </div>
+        <h2>Activer votre compte</h2>
+        <form method="post">
+            <p>Nouveau Mot de Passe :</p>
+            <input type="password" id="login" name="login">
+            <p>Confirmer Mot de Passe :</p>
+            <input type="password" id="pwd" name="pwd">
+            <input class="submit" type="submit" value="Connexion">
+        </form>
     </div>
+          
 
 </body>
 
